32ba96f44da968d75f429a1f8e162866a2196d67
Mateusz <matyd356@gmail.com>
2021-03-01 22:38:01 +0100

    add log in & log out features

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const moongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = moongoose.Schema;
 
 const mongoDb = process.env.MONGO_DB_LINK;
 
@@ -29,7 +29,61 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+app.get('/sign-up', (req, res) => res.render('sign-up-form'))
+app.post('/sign-up', (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  }).save(err => {
+    if (err) {
+      return next(err)
+    };
+    res.redirect('/');
+  });
+})
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+app.get("/log-out", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return doesNotReject(null, false, { message: 'Incorrect username' })
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' })
+      }
+      return done(null, user)
+    }
+    )
+  })
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
 
 app.listen(3000, () => console.log('app listenig on port 3000! '))
